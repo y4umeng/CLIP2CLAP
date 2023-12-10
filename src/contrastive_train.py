4796a8f2d1fa2537ac74e328b06b404ab9173206
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2023-12-10 18:37:17 -0500

    added contrastive euclid loss func

--- src/contrastive_train.py
+++ src/contrastive_train.py
@@ -4,7 +4,7 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor, transforms
 import torch.nn as nn
 
-def calc_loss(pred, yb, t):
+def calc_loss_cos_similarity(pred, yb, t):
   logits = torch.mm(pred, torch.t(yb)) * torch.exp(t)
   labels = torch.arange(yb.shape[0]).to("cuda")
   loss_1 = nn.CrossEntropyLoss()(logits, labels)
@@ -12,7 +12,15 @@ def calc_loss(pred, yb, t):
   loss_2 = nn.CrossEntropyLoss()(logits, labels)
   return (loss_1 + loss_2)/2
 
-def train_contrastive_model(train_dl, test_dl, model, optimizer, scheduler, num_epochs, model_name):
+def calc_loss_euclid(pred, yb, t):
+  logits = torch.pow(torch.cdist(pred, yb, p=2), -1) * torch.exp(t)
+  labels = torch.arange(yb.shape[0]).to("cuda")
+  loss_1 = nn.CrossEntropyLoss()(logits, labels)
+  torch.t(logits)
+  loss_2 = nn.CrossEntropyLoss()(logits, labels)
+  return (loss_1 + loss_2)/2
+
+def train_contrastive_model(train_dl, test_dl, model, optimizer, scheduler, num_epochs):
   t = nn.Parameter(torch.tensor([0.0])).to("cuda")
   for epoch in range(num_epochs):
     model.train()
@@ -24,7 +32,7 @@ def train_contrastive_model(train_dl, test_dl, model, optimizer, scheduler, num_
       pred = model(xb)
 
       # contrastive loss
-      loss = calc_loss(pred, yb, t)
+      loss = calc_loss_euclid(pred, yb, t)
 
       optimizer.zero_grad()
       loss.sum().backward()
@@ -33,7 +41,7 @@ def train_contrastive_model(train_dl, test_dl, model, optimizer, scheduler, num_
       train_loss += loss
       num_data_points += xb.shape[0]
 
-      if num_batch % 20 == 0:
+      if num_batch % 500 == 0:
         print(f"Epoch: {epoch} Batch: {num_batch} Batch Loss: {loss}")
       num_batch += 1
 
@@ -42,25 +50,17 @@ def train_contrastive_model(train_dl, test_dl, model, optimizer, scheduler, num_
     model.eval()
     test_loss = 0.0
     num_data_points = 0
-    num_batch = 0
     for xb, yb in test_dl:
       #get model predictions [B, d_model=512]
       pred = model(xb)
 
       # contrastive loss
-      loss = calc_loss(pred, yb, t)
-
-      optimizer.zero_grad()
-      loss.sum().backward()
-      optimizer.step()
+      loss = calc_loss_euclid(pred, yb, t)
 
       test_loss += loss
       num_data_points += xb.shape[0]
-      num_batch += 1
-      if num_batch == 100:
-        break
 
     print(f"Test average loss after epoch {epoch} is {test_loss/num_data_points}")
 
     #save model
-    torch.save(model.state_dict(), f"./checkpoints/{model_name}_epoch_{epoch}.pt")
+    torch.save(model.state_dict(), f"./checkpoints/linear_contrastive_epoch_{epoch}.pt")
