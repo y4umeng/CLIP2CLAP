f873e32308253d1ae15e1653c62d15dba4f3a906
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2023-12-13 11:37:53 -0500

    changing margin

--- src/contrastive_train.py
+++ src/contrastive_train.py
@@ -38,7 +38,7 @@ def calc_loss_euclid(pred, yb, accuracy, margin):
   return (loss_1 + loss_2)/2, (accuracy_1 + accuracy_2)/2
 
 def train_contrastive_model(train_dl, test_dl, model, optimizer, scheduler, num_epochs, model_name, start_epoch):
-  margin = nn.Parameter(torch.tensor([1.0])).to("cuda")
+  margin = 0.7
   torch.autograd.set_detect_anomaly(True)
   for epoch in range(start_epoch, start_epoch + num_epochs):
     print(f"EPOCH {epoch} BEGINS")
@@ -69,7 +69,7 @@ def train_contrastive_model(train_dl, test_dl, model, optimizer, scheduler, num_
       num_data_points += batch_size
 
       if num_batch % 1 == 0:
-        print(f"Epoch: {epoch} Batch: {num_batch} Avg Loss: {loss/batch_size} Avg Accuracy: {acc} Nans: {nans} Margin: {margin.item()}")
+        print(f"Epoch: {epoch} Batch: {num_batch} Avg Loss: {loss/batch_size} Avg Accuracy: {acc} Nans: {nans}")
 
       num_batch += 1
 
@@ -99,3 +99,7 @@ def train_contrastive_model(train_dl, test_dl, model, optimizer, scheduler, num_
 
     print(f"\nTest average loss after epoch {epoch} is {test_loss/num_data_points}")
     print(f"Test average accuracy after epoch {epoch} is {test_acc/num_data_points}\n")
+
+    if margin < 1.0:
+      margin += 0.05
+    print(f"Margin is now {margin}.\n")
