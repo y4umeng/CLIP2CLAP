9cf09a0671cb74c2358ed63a8b7074a8096ed8cf
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2023-12-12 19:48:00 -0500

    contrastive encoder

---
+++ src/encoder4.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+class AttentionAligner4(nn.Module):
+    def __init__(self, clip_embed, d_model=96):
+        super().__init__()
+
+        self.ff1 = nn.Linear(1, d_model)
+        self.ff2 = nn.Linear(d_model, 1)
+        self.pos_emb = nn.Embedding(clip_embed, d_model)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model, nhead=8, dim_feedforward=96)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=8)
+
+
+    def forward(self, x):
+        x.unsqueeze_(-1)
+
+        # B, T, C
+        B, T, C = x.shape
+
+        assert(T == 512)
+        assert(C == 1)
+
+        # B, 512, 1 
+        x = self.ff1(x)
+
+        # B, 512, d_model
+        pos_emb = self.pos_emb(torch.arange(T, device="cuda"))
+
+        # B, 512, d_model
+        x = x + pos_emb
+
+        x = self.transformer_encoder(x)
+
+        logits = self.ff2(x).squeeze(-1)
+        
+        return logits
+
+
+
+
+
---
+++ src/encoder_contrastive.py
@@ -0,0 +1,54 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, transforms
+import torch.nn as nn
+import open_clip
+from diffusers import AudioLDMPipeline
+from data import get_models, get_data, get_embeds, EmbeddingsDataset
+from contrastive_train import train_contrastive_model
+from encoder4 import AttentionAligner4
+
+def train():
+    _, _, _ = get_models()
+    train_data, test_data = get_data()
+    train_dataset = EmbeddingsDataset(train_data)
+    test_dataset = EmbeddingsDataset(test_data)
+    print(f"Length of test data: {len(test_dataset)}")
+    print(f"Length of train data: {len(train_dataset)}")
+
+    torch.manual_seed(420)
+ 
+    # Hyperparameters.
+    batch_size = 32
+    EPOCHS = 40
+    LR = 0.001
+    device = 'cuda'
+    clip_embed_dim = 512
+    clap_embed_dim = 512
+
+    print(f"Batch size: {batch_size}")
+    print(f"Epochs: {EPOCHS}")
+    print(f"LR: {LR}")
+    print(f"CLIP embed: {clip_embed_dim}")
+    print(f"CLAP embed: {clap_embed_dim}")
+
+    train_dl = DataLoader(train_dataset, batch_size, shuffle=True)
+    test_dl = DataLoader(test_dataset, batch_size, shuffle=True)
+
+    model = AttentionAligner4(clip_embed_dim)
+    model.to(device)
+    model = nn.DataParallel(model)
+    # checkpoint = torch.load("../checkpoints/linear_contrastive_loss_epoch_8.pt")
+    # model.load_state_dict(checkpoint)
+    model.train()
+
+    # Print the number of parameters in the model
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    print(f"Number of parameters: {sum([torch.prod(torch.tensor(p.shape)) for p in model_parameters])}")
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=LR)
+
+    train_contrastive_model(train_dl, test_dl, model, optimizer, None, EPOCHS, "encoder_contrastive_512")
+
+train()
