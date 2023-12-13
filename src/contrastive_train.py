66e3d555342363575f0f3850e34cb3d305f2e7d6
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2023-12-13 11:02:42 -0500

    finding ideal margin

--- src/contrastive_train.py
+++ src/contrastive_train.py
@@ -17,6 +17,9 @@ def calc_loss_euclid(pred, yb, t, accuracy, margin):
   # calculate distance metric
   logits = torch.cdist(pred, yb, p=2)
   logits = torch.pow(logits, -1) * torch.exp(t)
+
+  print(f"Logit mean: {torch.mean(logits)}")
+  print(f"Logit stddev: {torch.dim(logits)}")
   
   # apply margin
   logits = nn.ReLU()(logits - margin) + margin
@@ -63,8 +66,8 @@ def train_contrastive_model(train_dl, test_dl, model, optimizer, scheduler, num_
       train_acc += acc * batch_size
       num_data_points += batch_size
 
-      if num_batch % 500 == 0:
-        print(f"Epoch: {epoch} Batch: {num_batch} Avg Loss: {loss/batch_size} Avg Accuracy: {acc} Nans: {nans} Margin: {margin.item()}")
+      if num_batch % 1 == 0:
+        print(f"Epoch: {epoch} Batch: {num_batch} Avg Loss: {loss/batch_size} Avg Accuracy: {acc} Nans: {nans} Margin: {margin.item()} Temp: {temperature}")
 
       num_batch += 1
 
