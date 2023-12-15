5705aa65ed1daf0795222e15e44cee4624b675c4
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2023-12-15 11:03:26 -0500

    testing encoder

--- src/encoder4.py
+++ src/encoder4.py
@@ -2,14 +2,14 @@ import torch
 import torch.nn as nn
 
 class AttentionAligner4(nn.Module):
-    def __init__(self, clip_embed, d_model=1):
+    def __init__(self, clip_embed, d_model=8):
         super().__init__()
 
         self.linear = nn.Linear(512, 512)
         self.ff1 = nn.Linear(1, d_model)
         self.ff2 = nn.Linear(d_model, 1)
         self.pos_emb = nn.Embedding(clip_embed, d_model)
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model, nhead=8, dim_feedforward=96)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model, nhead=4, dim_feedforward=8)
         self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=8)
 
 
